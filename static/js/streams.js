7751136631523792f4b8de73437722a88cebb23e
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-03-23 01:18:37 +0500

    AgoraSDK setup and related work

---
+++ static/js/streams.js
@@ -0,0 +1,5 @@
+const APP_ID = 'c0b140fce0e844d1b9795dba24e24d3b'
+const CHANNEL = 'main'
+const TOKEN = '006c0b140fce0e844d1b9795dba24e24d3bIAAuITf9xxWEuVowwRGULdX/sKe8ounLSWJwEQdKSD6DRWTNKL8AAAAAEABqGbu4Vn07YgEAAQBWfTti'
+
+console.log('Stream.js connected')
