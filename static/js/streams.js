2694456ffc5297e754f06da15a814cadcb001d4a
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-03-23 02:14:35 +0500

    AgoraRTC setup, created,set,play and publish player

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,5 +1,35 @@
 const APP_ID = 'c0b140fce0e844d1b9795dba24e24d3b'
 const CHANNEL = 'main'
 const TOKEN = '006c0b140fce0e844d1b9795dba24e24d3bIAAuITf9xxWEuVowwRGULdX/sKe8ounLSWJwEQdKSD6DRWTNKL8AAAAAEABqGbu4Vn07YgEAAQBWfTti'
+let UID;
 
 console.log('Stream.js connected')
+
+const client = AgoraRTC.createClient({mode:'rtc',codec:'vp8'})
+
+let localTracks=[]
+let remoteUsers={}
+
+let joinAndDisplayLocalStream = async() => {
+    // join channel
+    UID = await client.join(APP_ID,CHANNEL,TOKEN,null)
+
+    // get audio and video tracks
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+
+    // create a player
+    let player = `<div class="video-container" id="user-container-${UID}">
+                    <div class="username-wrapper"><span class="user-name">My Name</span></div> 
+                    <div class="video-player" id="user-${UID}"></div>
+                </div>`
+    //append the player to video-streams
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend',player)
+
+    // play the method
+    localTracks[1].play(`user-${UID}`)
+
+    //publish track
+    await client.publish([localTracks[0],localTracks[1]])
+}
+
+joinAndDisplayLocalStream()
