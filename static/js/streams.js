2625c76a06c16825959ee295680cd83a0036ad35
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-03-23 05:04:52 +0500

    adding and handling multiple users

--- static/js/streams.js
+++ static/js/streams.js
@@ -11,6 +11,11 @@ let localTracks=[]
 let remoteUsers={}
 
 let joinAndDisplayLocalStream = async() => {
+
+    // subscribe to event listener
+    // whenever user publishes their track, handleUserJoined is called
+    client.on('user-published', handleUserJoined)
+
     // join channel
     UID = await client.join(APP_ID,CHANNEL,TOKEN,null)
 
@@ -22,14 +27,51 @@ let joinAndDisplayLocalStream = async() => {
                     <div class="username-wrapper"><span class="user-name">My Name</span></div> 
                     <div class="video-player" id="user-${UID}"></div>
                 </div>`
+
     //append the player to video-streams
     document.getElementById('video-streams').insertAdjacentHTML('beforeend',player)
 
-    // play the method
+    // call the play method
     localTracks[1].play(`user-${UID}`)
 
     //publish track
     await client.publish([localTracks[0],localTracks[1]])
+}
+
+let handleUserJoined = async (user,mediaType)=>{
+
+    // adding user to remote user
+    remoteUsers[user.uid] = user
+
+    //local client subscribe to this user
+    await client.subscribe(user,mediaType)
+
+
+    if(mediaType==='video'){
+        //checking if player already exists(user rejoining refreshing) then we remove that player
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player!=null){
+            player.remove()
+        }
+
+        // create a player
+        player = `<div class="video-container" id="user-container-${user.uid}">
+                    <div class="username-wrapper"><span class="user-name">My Name</span></div> 
+                    <div class="video-player" id="user-${user.uid}"></div>
+                </div>`
+
+        //append the player to video-streams
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend',player)
+
+        // call the play method
+        console.log("subscribe video success");
+        user.videoTrack.play(`user-${user.uid}`)
+
+    }
+
+    if (mediaType==='audio'){
+        user.audioTrack.play()
+    }
 }
 
 joinAndDisplayLocalStream()
