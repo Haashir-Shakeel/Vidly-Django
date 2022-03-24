cc6df1fe5b8838c5b00b367edf5ce17dead94648
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-03-25 03:17:43 +0500

    adding model RoomMember

--- main/models.py
+++ main/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-#create database table RoomMember | username, uid, roomname
+#create database table RoomMember | username, uid, room name
 #whenever user joins, create entry
 #on handleUserJoin event,query db for room member name using uid
 #on leave, delete RoomMember
+
+class RoomMember(models.Model):
+    name = models.CharField(max_length=200)
+    uid = models.CharField(max_length=200)
+    room_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
