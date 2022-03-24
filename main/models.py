b9b76336f17cb1352c95d3c6af524553022951a7
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-03-25 03:11:15 +0500

    sending and getting 'username' in session

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+#create database table RoomMember | username, uid, roomname
+#whenever user joins, create entry
+#on handleUserJoin event,query db for room member name using uid
+#on leave, delete RoomMember
